f4226b348b148a22f81dd5fd223ee6da3c354651
Luke Curran <luke.peter.curran54@gmail.com>
2021-08-06 16:36:56 -0500

    luke-search

--- src/main/java/com/topteer/topteer/controllers/SearchServlet.java
+++ src/main/java/com/topteer/topteer/controllers/SearchServlet.java
@@ -1,5 +1,21 @@
 package com.topteer.topteer.controllers;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+
+@WebServlet("/search")
 public class SearchServlet {
-    // The Very Hungry Caterpillar
-}
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String query = req.getParameter("searchTitle");
+            List<Ad> results = DaoFactory.getAdsDao().searchByTitle(query);
+            req.setAttribute("query", query);
+            req.setAttribute("ads", results);
+            req.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(req, resp);
+        }
+    }
