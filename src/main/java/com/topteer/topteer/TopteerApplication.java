420ea8c958298ac808813b61f681ffe535d5abba
Evan Williams <evan.b.williams95@gmail.com>
2021-08-06 13:50:21 -0500

    intitialized

---
+++ src/main/java/com/topteer/topteer/TopteerApplication.java
@@ -0,0 +1,13 @@
+package com.topteer.topteer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopteerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TopteerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/topteer/topteer/TopteerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topteer.topteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopteerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
