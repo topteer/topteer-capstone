af3c941fa1663ffdc7cdd41ade82096c0d7d71fc
Luke Curran <luke.peter.curran54@gmail.com>
2021-08-20 14:59:27 -0500

    david finished our relationship hoodah

--- src/main/java/com/topteer/topteer/controllers/EventVolunteerController.java
+++ src/main/java/com/topteer/topteer/controllers/EventVolunteerController.java
@@ -1,5 +1,6 @@
 package com.topteer.topteer.controllers;
 
+import com.topteer.topteer.models.Events;
 import com.topteer.topteer.models.User;
 import com.topteer.topteer.repositories.EventRepository;
 import com.topteer.topteer.repositories.EventVolunteerRepo;
@@ -11,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class EventVolunteerController {
@@ -24,32 +27,15 @@ public class EventVolunteerController {
         this.EVRepo = EVRepoDao;
     }
 
-    @GetMapping("/event/{id}/register")
-    public String getRegisterButton(@PathVariable long id, Model model) {
-        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Long currentUserID = currentUser.getId();
-        model.addAttribute("currentUserID", currentUserID);
-//
-        model.addAttribute("eventID", id);
-        return "event/show";
-    }
-
     @PostMapping("/event/{id}/register")
     public String placeholder1(@PathVariable long id){
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        Long currentUserID = currentUser.getId();
-        EVRepo.save(e);
-
+        currentUser = userDao.getById(currentUser.getId());
+        Events event = eventDao.getById(id);
+        List<Events> userEvents=currentUser.getEvents();
+        userEvents.add(event);
+        currentUser.setEvents(userEvents);
+        userDao.save(currentUser);
+        return "redirect:/event/" + id + "/show";
     }
-
-
-//    @PostMapping("/event/{id}/register")
-//    public String postRegisterButton(@RequestParam long eventID, @RequestParam long currentUserID) {
-//        Events event = eventDao.getById(eventID);
-//        User user = userDao.getById(currentUserID);
-//        EVRepoDao.save(register);
-//
-//        return "redirect:/event";
-//    }
-
 }
