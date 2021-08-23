b24127a2d2b1d68ee15f8054fe80e3c6e0c5b59d
Luke Curran <luke.peter.curran54@gmail.com>
2021-08-23 16:55:45 -0500

    added some labels, identified whitelabels

--- src/main/java/com/topteer/topteer/controllers/EventsController.java
+++ src/main/java/com/topteer/topteer/controllers/EventsController.java
@@ -67,11 +67,10 @@ public class EventsController {
 
             Events event = new Events(org, title, description, user, phone, date, time, location, hours, length);
             eventDao.save(event);
-
         }
-
         return "redirect:/event";
     }
+
     @GetMapping("/event/{id}/show")
     public String singleEvent(@PathVariable long id, Model model){
         Events events = eventDao.getById(id);
@@ -85,16 +84,15 @@ public class EventsController {
         model.addAttribute("event", events);
         model.addAttribute("eCoord", eventCoord);
         model.addAttribute("isEventOwner", isEventOwner);
-        return "event/show";
+        return "event/{id}/show";
     }
 
     @PostMapping("/event/{id}/delete")
     public String deleteEvent(@PathVariable long id){
         Events eventFromDb = eventDao.getById(id);
-        eventFromDb.setEventvolunteer(new ArrayList<User>());
+        eventFromDb.setEventvolunteer(new ArrayList<User >());
         eventDao.save(eventFromDb);
         eventDao.delete(eventFromDb);
         return "redirect:/event";
     }
-
 }
