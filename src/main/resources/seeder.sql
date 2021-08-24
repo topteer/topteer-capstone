cafd5da5f1c3eed411e30cae2d3c93457e4ba713
Evan Williams <evan.b.williams95@gmail.com>
2021-08-24 15:51:44 -0500

    added a seeder file

---
+++ src/main/resources/seeder.sql
@@ -0,0 +1,10 @@
+USE top_teer_db;
+
+INSERT INTO top_teer_db.users (id, email, first_name, last_name, password, username)
+values ('3','billbob@place.com','bill','bob','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','billybob'),
+         ('4','roger@place.com','Roger','Sandoval','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Roger'),
+         ('5','rick.sanchez@place.com','Rick','Sanchez','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Rick'),
+         ('6','morty@place.com','Morty','Smith','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Morty'),
+         ('7','joe@place.com','Joe','Hopkins','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Joe'),
+         ('8','jeff@place.com','Jeff','Rowe','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Jeff'),
+         ('9','Luke@place.com','Luke','Curran','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Luke');
