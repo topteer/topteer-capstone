50df64360765d0e45e361fd482a62da3ce5f53cc
Evan Williams <evan.b.williams95@gmail.com>
2021-08-30 14:53:18 -0500

    added fixed scrolling window to results page

--- src/main/resources/seeder.sql
+++ src/main/resources/seeder.sql
@@ -7,3 +7,11 @@ values ('billbob@place.com','bill','bob','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0K
          ('joe@place.com','Joe','Hopkins','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Joe'),
          ('jeff@place.com','Jeff','Rowe','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Jeff'),
          ('Luke@place.com','Luke','Curran','$2a$10$V/au1n2mV6378Q6wdh9XiO66kowGV0Kk2kbbdS0jRl5DMwxtfkKGW','Luke');
+
+
+INSERT INTO top_teer_db.events (date, description, end_time, hours, location, phone, start_time, title, org_id, e_coordid)
+
+values (2021,'test event 1', '1:00',6,'a place','2222222222','9:00','testing',1,1),
+ (2021,'test event 1', '1:00',6,'a place','2222222222','9:00','testing',1,1),
+(2021,'test event 2', '1:00',6,'a place','2222222222','9:00','testing2',1,1),
+(2021,'test event 3', '1:00',6,'a place','2222222222','9:00','testing3',1,1);
